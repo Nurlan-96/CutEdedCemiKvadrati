86022be5ac13143e3eda379700efc91854053aed
Nurlan <nurlanea@code.edu.az>
2023-11-02 19:52:06 +0400

    upload

---
+++ cutededkvadrati/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(count(10, 11, 12, 13, 14, 15, 16, 17));
+int count(params int[] a)
+{
+    int res = 0;
+    foreach (int i in a)
+    {
+        if (i % 2 == 0)
+        {
+            res += i;
+        }
+    }
+    return res*=res;
+}
